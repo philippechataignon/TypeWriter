e4969e3f441e6c721f780ac3a7e08f96a7ffc379
Philippe Chataignon <philippe@ldlc>
2020-08-12 11:31:12 +0200

    Add latin1 option

--- print.py
+++ print.py
@@ -15,13 +15,15 @@ def main():
     parser = argparse.ArgumentParser(description="Print on Panasonic R191")
     parser.add_argument("-p", "--port", type=str, help="COM port", default="/dev/ttyACM0")
     parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    parser.add_argument("-l", "--latin1", help="input is encoded in latin1 (default UTF8)", action="store_true")
     args = parser.parse_args()
     with serial.Serial(args.port, 9600) as ser:
         ser.timeout = 1
         wait_ready(ser)
         ser.timeout = 60
         for line in sys.stdin:
-            line = unicodedata.normalize("NFC", line).encode("latin1", "ignore")
+            if not args.latin1:
+                line = unicodedata.normalize("NFC", line).encode("latin1", "ignore")
             if args.verbose:
                 print(line)
             ser.write(line)

81157df1cbd6318d55134a4632c65239af35c3b4
Philippe Chataignon <philippe@ldlc>
2020-08-12 11:10:39 +0200

    Add verbose opt

--- print.py
+++ print.py
@@ -8,13 +8,13 @@ import unicodedata
 def wait_ready(ser):
     while True:
         read = ser.read()
-        print(read)
         if read == b".":
              break
 
 def main():
     parser = argparse.ArgumentParser(description="Print on Panasonic R191")
-    parser.add_argument("--port", metavar="port", type=str, help="COM port", default="/dev/ttyACM0")
+    parser.add_argument("-p", "--port", type=str, help="COM port", default="/dev/ttyACM0")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
     args = parser.parse_args()
     with serial.Serial(args.port, 9600) as ser:
         ser.timeout = 1
@@ -22,7 +22,8 @@ def main():
         ser.timeout = 60
         for line in sys.stdin:
             line = unicodedata.normalize("NFC", line).encode("latin1", "ignore")
-            print(line)
+            if args.verbose:
+                print(line)
             ser.write(line)
             wait_ready(ser)
 
